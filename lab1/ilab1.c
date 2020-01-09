371ec5e5692a69314bff4c5286e3fda7b444424a
prajwal27 <prajwalpspbelagavi@gmail.com>
2020-01-09 14:51:03 +0530

    lab1

---
+++ lab1/ilab1.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+
+int main() {
+    /* hey 
+    scbskdbn */   n   m;
+    return 0;
+}
---
+++ lab1/oLab1.c
@@ -0,0 +1,7 @@
+1. 
+2. #include<stdio.h>
+2. 
+3. intmain(){
+4. nm;
+5. return0;
+6. }
